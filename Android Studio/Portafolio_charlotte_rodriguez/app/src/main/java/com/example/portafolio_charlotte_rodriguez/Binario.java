d52477639d5bcfb7d9dca4a7ad7a29926c04fd2f
Charlotte Rodriguez <euphoria.head@gmail.com>
2022-10-05 21:03:46 -0300

    Terminado version semi finalizada primera entrega

--- Android Studio/Portafolio_charlotte_rodriguez/app/src/main/java/com/example/portafolio_charlotte_rodriguez/Binario.java
+++ Android Studio/Portafolio_charlotte_rodriguez/app/src/main/java/com/example/portafolio_charlotte_rodriguez/Binario.java
@@ -8,23 +8,53 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class Binario extends AppCompatActivity {
-    CheckBox checkBox1, checkBox2, checkBox3, checkBox4;
+    CheckBox checkBox128, checkBox64, checkBox32, checkBox16 ,checkBox8, checkBox4, checkBox2, checkBox1;
     TextView resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_binario);
         // No vincular más de un layout
-        checkBox1  = findViewById(R.id.cb_8);
-        checkBox2  = findViewById(R.id.cb_4);
-        checkBox3 = findViewById(R.id.cb_2);
-        checkBox4 = findViewById(R.id.cb_1);
+        checkBox128 = findViewById(R.id.cb_128);
+        checkBox64 = findViewById(R.id.cb_64);
+        checkBox32 = findViewById(R.id.cb_32);
+        checkBox16 = findViewById(R.id.cb_16);
+        checkBox8  = findViewById(R.id.cb_8);
+        checkBox4  = findViewById(R.id.cb_4);
+        checkBox2 = findViewById(R.id.cb_2);
+        checkBox1 = findViewById(R.id.cb_1);
         resultado = findViewById(R.id.mostrarTextoresultado);
 
     }
 
     public void transformar(View view) {
         int x = 0;
+
+        if (checkBox1.isChecked()){
+            x = x + 1 ;
+        }
+        if (checkBox2.isChecked()){
+            x = x + 2;
+        }
+        if(checkBox4.isChecked()){
+            x = x + 4;
+        }
+        if (checkBox8.isChecked()){
+            x = x + 8;
+        }
+        if (checkBox16.isChecked()){
+            x = x + 16;
+        }
+        if (checkBox32.isChecked()){
+            //Es la manera compacta de decir que x es igual a  osea  0  que es x +32
+            x += 32;
+        }
+        if (checkBox64.isChecked()){
+            x = x +64;
+        }
+        if (checkBox128.isChecked()){
+            x = x + 128;
+        }
         resultado.setText(String.valueOf(x));
     }
 }
--- Android Studio/Portafolio_charlotte_rodriguez/app/src/main/java/com/example/portafolio_charlotte_rodriguez/Checkbox.java
+++ Android Studio/Portafolio_charlotte_rodriguez/app/src/main/java/com/example/portafolio_charlotte_rodriguez/Checkbox.java
@@ -3,12 +3,52 @@ package com.example.portafolio_charlotte_rodriguez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Checkbox extends AppCompatActivity {
-
+    EditText numero1, numero2;
+    CheckBox sumar , restar;
+    TextView resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkbox);
+        numero1 = findViewById(R.id.etn1);
+        numero2 = findViewById(R.id.etn2);
+        sumar = findViewById(R.id.cb_sumar_checkbox);
+        restar = findViewById(R.id.cb_restar_checkbox);
+        resultado = findViewById(R.id.checkboxResultado);
+    }
+    public void calcular(View view){
+        //Esto obtiene el string dentro del elemento int
+        String obtenerElemento1 = numero1.getText().toString();
+        String obtenerElemento2 = numero2.getText().toString();
+
+        //Si uno no tiene ningun elemento añadido no hacer nada y devolver
+        if (obtenerElemento1.equals("") || obtenerElemento2.equals("")){
+            return;
+        }
+
+        //Pasar a  entero
+        int variable1 = Integer.parseInt(obtenerElemento1);
+        int variable2 = Integer.parseInt(obtenerElemento2);
+
+
+        if (restar.isChecked()&& sumar.isChecked()){
+            int resultadosuma= variable1 + variable2;
+            int resultadoresta = variable1 - variable2;
+            resultado.setText(String.valueOf(resultadosuma)+"// "+String.valueOf(resultadoresta));
+        }
+        else if (sumar.isChecked()){
+            int resultadosuma =   variable1 + variable2;
+            resultado.setText(String.valueOf(resultadosuma));
+        }
+        else if (restar.isChecked()){
+            int resultadosuma =  variable1 - variable2;
+            resultado.setText(String.valueOf(resultadosuma));
+        }
     }
 }
