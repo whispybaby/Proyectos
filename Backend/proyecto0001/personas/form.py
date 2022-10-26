1140c8733b613a9393ccba74175ea7e7df7ec204
Charlotte Rodriguez <euphoria.head@gmail.com>
2022-10-26 20:08:35 -0300

    avance clase  de backend antes de realizar tarea

--- Backend/proyecto0001/personas/form.py
+++ Backend/proyecto0001/personas/form.py
@@ -4,6 +4,31 @@ from .models import Personas
 
 class FormPersona(forms.ModelForm):
     class Meta:
+        #Modelo 
         model = Personas
-        fields = ['run', 'nombre', 'fecha_nacimiento',
-                     'estado_civil', 'genero', 'direccion', 'cometario', 'fono']
+        #inputs que se mostraran en el formulario
+        fields = '__all__'
+    eCivil = [('Casada/o' ,'Casad@'),('Solter@' ,'Soltera/o'),('Viuda/o' ,'Viud@'),
+                    ('Separada/o', 'Separad@')]
+    
+    direccion = forms.CharField(widget=forms.TextInput(
+        attrs={'class':'form-control'}))
+    nombre = forms.CharField(widget=forms.TextInput(
+        attrs={'class':'form-control'}))
+    genero = forms.CharField(widget=forms.TextInput(
+        attrs={'class':'form-control'}))
+    fono = forms.CharField(widget=forms.TextInput(
+        attrs={'class':'form-control'}))
+    run = forms.CharField(widget=forms.TextInput(
+        attrs={'class':'form-control'}))
+    fecha_nacimiento = forms.DateField(
+        widget=forms.DateInput(
+            attrs={'type':'date','class':'form-control'}
+            
+    ))
+  
+    cometario = forms.CharField(widget=forms.TextInput(
+        attrs={'class':'form-control'}))
+
+    estado_civil = forms.CharField(widget=forms.Select(choices=eCivil,
+    attrs={'class': 'form-control'}))
--- Backend/proyecto0001/personas/views.py
+++ Backend/proyecto0001/personas/views.py
@@ -1,13 +1,17 @@
-from ast import If
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from personas import form
 from personas.form import FormPersona
+from personas.models import Personas
+from django.urls import reverse
+
 
 # Create your views here.
 def index(request):
     #instanciar la clase Formpersona
-    
-    return render(request, 'personas/index.html' )
+    personas = Personas.objects.all()
+    return render(request, 'personas/index.html', {
+        'personas':personas
+    } )
 
 def registrar (request):
     formulario = FormPersona()
@@ -18,11 +22,34 @@ def registrar (request):
         formulario = FormPersona(request.POST)
         if formulario.is_valid(): # linea que seleciona la validacion
             formulario.save()
-            print('Valido')
-        else:
-            print('No valido')
+            return redirect(reverse('personas:index'))
     else:
         print("Porfavor ingrese un valor para que debe se ser valido")
     return render(request, 'personas/create.html', {
         'form': formulario
+
     })
+
+def editarPersona(request, run):
+    # variable que guarde el registro con el run solicitado
+    p = Personas.objects.get(run = run)
+    # los datos del registro se asignan al formulario
+    form = FormPersona(instance = p)
+    # ingresa al if una vez presionado el btn guardar
+    if request.method == 'POST':
+        #enviando los datos modificados
+        form = FormPersona(request.POST,instance=p)
+        #verifica si los datos son validos
+        if form.is_valid():
+            #guarda
+            form.save()
+            return redirect(reverse('personas:index'))
+    data = {'form':form,'titulo':'Actualizar Persona'}
+    return render(request, 'personas/create.html', data)
+
+
+def borrarPersona(request, run):
+    p = Personas.objects.get(run = run)
+    p.delete()
+    # los datos del registro se asignan al formulario
+    return redirect(reverse('personas:index'))
